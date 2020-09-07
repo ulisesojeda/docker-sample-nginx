f1bca95b110fa089135a8504c7ff64bb7a232089
Ulises Ojeda <ulises.odysseus22@gmail.com>
2020-09-07 16:51:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-sudo docker build -t nh_33 .
+sudo docker build -t ng_p1 .
 sudo docker kill `cat docker.cid`
 sudo rm docker.cid
-sudo docker run --cidfile docker.cid -p 8080:80 -d nh_33
+sudo docker run --cidfile docker.cid -p 8080:80 -d ng_p1
 echo $?
